3599bcd187947ecd684683324e7eedd0543245f1
kishansri9 <150356908+kishansri9@users.noreply.github.com>
2023-11-29 16:12:17 -0500

    ok

---
+++ js/script.js
@@ -0,0 +1,16 @@
+<script>
+(() => {
+  const modelViewer = document.querySelector('#orbit-demo');
+  const orbitCycle = [
+    '45deg 55deg 4m',
+    '-60deg 110deg 2m',
+    modelViewer.cameraOrbit
+  ];
+
+  setInterval(() => {
+    const currentOrbitIndex = orbitCycle.indexOf(modelViewer.cameraOrbit);
+    modelViewer.cameraOrbit =
+        orbitCycle[(currentOrbitIndex + 1) % orbitCycle.length];
+  }, 3000);
+})();
+</script>
